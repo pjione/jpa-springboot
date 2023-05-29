2cb644bd91715a6464184cdfd3cad2a07101e78d
pjione <a41924250@gmail.com>
2023-05-29 23:43:35 +0900

    JPA, DB 설정 및 동작확인

---
+++ src/test/java/jpa/hello/MemberRepositoryTest.java
@@ -0,0 +1,36 @@
+package jpa.hello;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@Rollback(false)
+class MemberRepositoryTest {
+    @Autowired
+    MemberRepository memberRepository;
+    @Test
+    void save() {
+        //given
+        Member member = new Member();
+        member.setUsername("member1");
+        //when
+        Long saveId = memberRepository.save(member);
+        Member findMember = memberRepository.find(saveId);
+        //then
+        assertThat(findMember.getId()).isEqualTo(member.getId());
+        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        assertThat(findMember).isEqualTo(member);
+    }
+
+    @Test
+    void find() {
+    }
+}
