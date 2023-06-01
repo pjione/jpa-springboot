73457bb1e6b8a60b060c9981b5b0f2b0c28fce49
pjione <a41924250@gmail.com>
2023-06-01 17:23:23 +0900

    회원 관련 컨트롤러, 뷰

---
+++ src/main/java/jpa/hello/controller/HomeController.java
@@ -0,0 +1,17 @@
+package jpa.hello.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequiredArgsConstructor
+@Slf4j
+public class HomeController {
+    @RequestMapping("/")
+    public String home(){
+        return "home";
+    }
+}
---
+++ src/main/java/jpa/hello/controller/MemberForm.java
@@ -0,0 +1,16 @@
+package jpa.hello.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@Setter
+public class MemberForm {
+    @NotBlank(message = "회원 이름은 필수 입니다.")
+    private String name;
+    private String city;
+    private String street;
+    private String zipcode;
+}
