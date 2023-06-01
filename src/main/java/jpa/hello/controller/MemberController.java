8bf3e5cd0dd6521582609d4ec1f40f78a974ce7a
pjione <a41924250@gmail.com>
2023-06-01 18:24:23 +0900

    상품 등록 뷰

--- src/main/java/jpa/hello/controller/MemberController.java
+++ src/main/java/jpa/hello/controller/MemberController.java
@@ -39,6 +39,6 @@ public class MemberController {
     public String list(Model model){
         List<Member> members = memberService.findMembers();
         model.addAttribute("members", members);
-        return "/members/memberList";
+        return "members/memberList";
     }
 }
--- src/main/java/jpa/hello/controller/MemberForm.java
+++ src/main/java/jpa/hello/controller/MemberForm.java
@@ -1,6 +1,8 @@
 package jpa.hello.controller;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.validation.constraints.NotBlank;
