dd61db9478e7d47920906613c8c6f11f36c67733
pjione <a41924250@gmail.com>
2023-06-02 16:15:44 +0900

    회원 조회 API memberDto를 List로 바로 반환하지 않고 오브젝트로 감싸서 리턴

--- src/main/java/jpa/hello/api/MemberApiController.java
+++ src/main/java/jpa/hello/api/MemberApiController.java
@@ -8,6 +8,8 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.constraints.NotEmpty;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -33,6 +35,24 @@ public class MemberApiController {
         memberService.update(id, request.getName());
         Member member = memberService.findMember(id).orElseThrow();
         return new UpdateMemberResponse(member.getId(), member.getName());
+    }
+    @GetMapping("/api/v2/members")
+    public Result memberV2(){
+        List<Member> members = memberService.findMembers();
+        List<MemberDto> collect = members.stream()
+                .map(m -> new MemberDto(m.getName()))
+                .collect(Collectors.toList());
+        return new Result(collect);
+    }
+    @Data
+    @AllArgsConstructor
+    static class Result<T>{
+        private T data;
+    }
+    @Data
+    @AllArgsConstructor
+    static class MemberDto{
+        private String name;
     }
 
     @Getter
